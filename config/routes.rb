7f661a52fff4b9209abf2ccb50f13f57e7eee95c
Hugo Revel <hugorevel@gmail.com>
2016-11-15 15:52:56 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #read all tasks
+  get "tasks" => "tasks#index"
+  # create a task
+    # - Afficher le formulaire
+  get "tasks/new" => "tasks#new", as: :new_task
+    # - Poster la nouvelle task
+  post "tasks" => "tasks#create"
+  # read one task
+  get "tasks/:id" => "tasks#show", as: :task
+
+
+  #update a task
+   #- Afficher le formulaire
+  get "tasks/:id/edit" => "tasks#edit", as: :edit_task
+  patch "tasks/:id" => "tasks#update"
+
+  #destroy a task
+  delete "tasks/:id" => "tasks#destroy", as: :destroy_task
+
+
 end
